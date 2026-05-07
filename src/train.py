0782f111a8f0f114b990959b1bccf766a0063a54
silvanus jaithan <kenkuy500@gmail.com>
2026-05-08 00:34:46 +0700

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,1 +1,21 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import joblib
 
+# Load dataset
+df = pd.read_csv("data/sales.csv")
+
+# Features
+X = df[['advertising', 'holiday', 'temperature', 'customers']]
+
+# Target
+y = df['sales']
+
+# Train model
+model = LinearRegression()
+model.fit(X, y)
+
+# Save model
+joblib.dump(model, "model/model.pkl")
+
+print("Model trained successfully!")
